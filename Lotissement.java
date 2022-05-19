b7923e077594c857d44ea9ff6a17a48f944042c3
eridra <104929157+eridra@users.noreply.github.com>
2022-05-19 01:29:11 -0400

    Update Lotissement.java
    nouvelles variables

--- Lotissement.java
+++ Lotissement.java
@@ -8,12 +8,17 @@ import net.sf.json.JSONObject;
  * @author 
  */
 public class Lotissement {
+    //Valeurs recues
     String description;
     int nbServices;
     int nbDroitsPassages;
     int superficie;
-    String dateMesure; //Inutile a ce que je comprend de la sortie attendue
-    double valeur;
+    String dateMesure; 
+    //Valeurs calculees
+    double valeurParSuperficie;
+    double valeurDroitPassage;
+    double montantServices;
+    double valeurParLot;
     
     public Lotissement(JSONObject source){
         
