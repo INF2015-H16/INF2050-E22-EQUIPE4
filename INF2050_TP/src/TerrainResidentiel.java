38f61a14f60e44c4994dd58d1adad7197d3e5a9e
Steny-11 <104903043+Steny-11@users.noreply.github.com>
2022-05-18 22:13:47 -0400

    First commit

---
+++ INF2050_TP/src/TerrainResidentiel.java
@@ -0,0 +1,29 @@
+
+package evaluationfonciere;
+
+/**
+ *
+ * @author 
+ */
+public class TerrainResidentiel extends Terrain {
+
+    public TerrainResidentiel(JSONObject source){
+
+    }
+
+    private double calculValeurLot(){
+        
+    }
+
+    private double calculMontantServices(int nbrServices){
+        
+    }
+
+    private double calculMontantDroitsPassage(){
+
+    }
+
+    private double calculMontantSuperficie(){
+
+    }
+}
