30b22e32e5b658e1612e94cbf79f8ee9009500ad
Steny-11 <104903043+Steny-11@users.noreply.github.com>
2022-05-19 09:35:31 -0400

    Constructeur Lotissement et getters/setters

--- Lotissement.java
+++ Lotissement.java
@@ -10,24 +10,63 @@ import net.sf.json.JSONObject;
 public class Lotissement {
     //Valeurs recues
     private String description;
-    private int nbServices;
     private int nbDroitsPassages;
+    private int nbServices;
     private int superficie;
     private String dateMesure; 
     //Valeurs calculees
-    private double valeurParSuperficie;
-    private double valeurDroitPassage;
+    private double valeurSuperficie;
+    private double montantDroitsPassage;
     private double montantServices;
-    private double valeurParLot;
+    private double valeurLot;
     
     public Lotissement(JSONObject source){
-    //initiliser seulement les valeurs recues    
+        description = source.getString("description");
+        nbDroitsPassages = source.getInt("nombre_droits_passage");
+        nbServices = source.getInt("nombre_services");
+        superficie = source.getInt("superficie");
+        dateMesure = source.getString("dateMesure");
     }
     
     //getters des valeurs recues
+    public String getDescription(){
+        return description;
+    }
+
+    public int getNbDroitsPassages(){
+        return nbDroitsPassages;
+    }
+
+    public int getNbServices(){
+        return nbServices;
+    }
     
+    public int getSuperficie(){
+        return superficie;
+    }
+
+    public int getDateMesure(){
+        return dateMesure;
+    }
+
     //setters des valeurs calculees
-    
+    public void setValeurSuperficie(double montant){
+        valeurSuperficie = montant;
+    }
+
+    public void setMontantDroitsPassage(double montant){
+        montantDroitsPassage = montant;
+    }
+
+    public void setMontantServices(double montant){
+        montantServices = montant;
+    }
+
+    public void setValeurLot(){
+        valeurLot = valeurSuperficie + montantDroitsPassage + montantServices;
+    }
+
+
     public JSONObject rapport(){
     //append les lignes demandees
     }
