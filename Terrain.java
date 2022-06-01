a603aad33d0a44d7b26ac18fa041b9cab7ffbaf2
Steny-11 <104903043+Steny-11@users.noreply.github.com>
2022-06-01 19:25:30 -0400

    Ajout methode arrondir et getters pour rapport

--- Terrain.java
+++ Terrain.java
@@ -1,6 +1,7 @@
 
 package evaluationfonciere;
 
+import java.text.DecimalFormal;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
@@ -17,64 +18,85 @@ public class Terrain {
     double taxeMunicipale;
     Lotissement [] lotissements;
     
-    final double prixFixe = 733.77;
-    final double montantBase = 500;
-    final double tauxScolaire = 0.012;
-    final double tauxMunicipale = 0.025;
+    static final double PRIX_FIXE = 733.77;
+    static final double TAUX_SCOLAIRE = 0.012;
+    static final double TAUX_MUNICIPALE = 0.025;
     
     public Terrain(JSONObject JSONSource){
         this.prixMin = Double.parseDouble(JSONSource.getString("prix_m2_min").split(" ")[0]);
         this.prixMax = Double.parseDouble(JSONSource.getString("prix_m2_max").split(" ")[0]);
         this.lotissements = formaterLot(JSONSource.getJSONArray("lotissements"));
+
+        switch(JSONSource.getInt("type_terrain")) {
+            case 0:
+                this.agricole();
+                break;
+            case 1:
+                this.residentiel();
+                break;
+            case 2:
+                this.commercial();
+                break;
+        }
     }
     
-    void residentiel() {
+    private void residentiel() {
         for(Lotissement lot : lotissements){
             lot.residentiel(prixMax, prixMin);
         }
     }
 
-    void agricole() {
+    private void agricole() {
         for(Lotissement lot : lotissements){
             lot.agricole(prixMin);
         }
     }
 
-    void commercial() {
+    private void commercial() {
         for(Lotissement lot : lotissements){
             lot.commercial(prixMax);
         }
     }
     
     public JSONObject rapport(){
-        valeurFonciereTotale = prixFixe;
+        valeurFonciereTotale = PRIX_FIXE;
 
         String pattern = "#.##";
         DecimalFormat decimalFormat = new DecimalFormat(pattern);
 
         for(Lotissement lot : lotissements){
-            valeurFonciereTotale += lot.valeurLot;
+            valeurFonciereTotale += lot.getValeurLot();
         }
         
-        taxeScolaire = valeurFonciereTotale * tauxScolaire;
-        taxeMunicipale = valeurFonciereTotale * tauxMunicipale;
+        //Arrondir ses montants au 5 sous 
+        valeurFonciereTotale = arrondiAu5sousSuperieur(valeurFonciereTotale);
+        taxeScolaire = arrondiAu5sousSuperieur(valeurFonciereTotale * TAUX_SCOLAIRE);
+        taxeMunicipale = arrondiAu5sousSuperieur(valeurFonciereTotale * TAUX_MUNICIPALE);
         
         JSONObject rapport = new JSONObject();
-        rapport.accumulate("valeur_fonciere_totale", decimalFormat.format(valeurFonciereTotale) + " " + "$");
-        rapport.accumulate("taxe_scolaire", decimalFormat.format(taxeScolaire) + " " + "$");
-        rapport.accumulate("taxe_ municipale", decimalFormat.format(taxeMunicipale) + " " + "$");
+        rapport.accumulate("valeur_fonciere_totale", decimalFormat.format(valeurFonciereTotale) + " $");
+        rapport.accumulate("taxe_scolaire", decimalFormat.format(taxeScolaire) + " $");
+        rapport.accumulate("taxe_ municipale", decimalFormat.format(taxeMunicipale) + " $");
         
         JSONArray lots = new JSONArray();
         for(Lotissement lot : lotissements){
             JSONObject lotUnique = new JSONObject();
-            lotUnique.accumulate("description", lot.description);
-            lotUnique.accumulate("valeur_par_lot", lot.valeurLot);
+            lotUnique.accumulate("description", lot.getDescription());
+
+            //Enoncer ne precise pas qu'il faut arrondir le montant au 5 sous pour chaque lot,
+            //mais l'exemple de sortie montre des montants arrondis
+            lotUnique.accumulate("valeur_par_lot", decimalFormat.format(lot.getValeurLot()) + " $"); 
+
             lots.add(lotUnique);
         }
         
         rapport.accumulate("lotissements", lots);
         
         return rapport;
+    }
+
+    public double arrondiAu5sousSuperieur(double montant) {
+        return Math.ceil(montant*20)/20;
     }
     
     //Copier coller de la methode a Eric
