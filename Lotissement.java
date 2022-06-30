0ab923d254f28e22e447a8731b9c1956dce00e6d
wildgolem <chhun.goldlen@courrier.uqam.ca>
2022-06-30 12:47:44 -0400

    ajout d'une classe verificateur

--- Lotissement.java
+++ Lotissement.java
@@ -5,7 +5,8 @@ import net.sf.json.JSONObject;
 
 /**
  *
- * @author Leonid
+ * @author  Leonid
+ *          Goldlen
  */
 public abstract class Lotissement{
     //Constantes
@@ -30,51 +31,11 @@ public abstract class Lotissement{
     double prixMin;
 
     Lotissement(JSONObject JSONSource) {
-        setDescription(JSONSource);
-        setNbDroitsPassages(JSONSource);
-        setDateMesure(JSONSource);
-        setNbServices(JSONSource);
-        setSuperficie(JSONSource);
-    }
-
-    private void setDescription(JSONObject JSONSource) throws FormatInvalide{
-        try {
-            description = JSONSource.getString("description");
-        } catch (JSONException e) {
-            throw new FormatInvalide("La propriete <description> d'un lot est manquante dans le fichier d'entree");
-        }
-    }
-
-    private void setNbDroitsPassages(JSONObject JSONSource) throws FormatInvalide {
-        try {
-            nbDroitsPassages = JSONSource.getInt("nombre_droits_passage");
-        } catch (JSONException e) {
-            throw new FormatInvalide("La propriete <nombre_droits_passage> d'un lot est manquante dans le fichier d'entree");
-        }
-    }
-
-    private void setDateMesure(JSONObject JSONSource) throws FormatInvalide {
-        try {
-            dateMesure = JSONSource.getString("date_mesure");
-        } catch (JSONException e) {
-            throw new FormatInvalide("La propriete <date_mesure> d'un lot est manquante dans le fichier d'entree");
-        }
-    }
-
-    private void setNbServices(JSONObject JSONSource) throws FormatInvalide {
-        try {
-            nbServices = JSONSource.getInt("nombre_services") + NB_SERVICE_BASE;
-        } catch (JSONException e) {
-            throw new FormatInvalide("La propriete <nombre_services> d'un lot est manquante dans le fichier d'entree");
-        }
-    }
-
-    private void setSuperficie(JSONObject JSONSource) throws FormatInvalide {
-        try {
-            superficie = JSONSource.getInt("superficie");
-        } catch (JSONException e) {
-            throw new FormatInvalide("La propriete <superficie> d'un lot est manquante dans le fichier d'entree");
-        }
+        descriptionExiste(JSONSource, description);
+        nbDroitsPassagesExiste(JSONSource, nbDroitsPassages);
+        dateMesureExiste(JSONSource, dateMesure);
+        nbServicesExiste(JSONSource, nbServices, NB_SERVICE_BASE);
+        superficieExiste(JSONSource, superficie);
     }
     
     void setPrixMinMax(double[] prixMinMax){
@@ -90,23 +51,5 @@ public abstract class Lotissement{
 
     public double getValeurTotalLot() {
         return valeurTotalLot;
-    }
-
-    public void verifierValeurs() throws FormatInvalide {
-        if(nbDroitsPassages > 10){
-            throw new FormatInvalide("Le nombre de droits de passage est superieur a 10");
-        } else if(nbDroitsPassages < 0){
-            throw new FormatInvalide("Le nombre de droits de passage est inferieur a 0");
-        }
-        if(nbServices > 5){
-            throw new FormatInvalide("Le nombre de services est superieur a 5");
-        } else if(nbServices > 5){
-            throw new FormatInvalide("Le nombre de services est inferieur a 0");
-        }
-        if(superficie > 50000){
-            throw new FormatInvalide("La superficie ne peut pas etre supérieure a 50000 metres carres");
-        } else if(superficie < 0){
-            throw new FormatInvalide("La superficie ne peut pas etre négative");
-        }
     }
 }
