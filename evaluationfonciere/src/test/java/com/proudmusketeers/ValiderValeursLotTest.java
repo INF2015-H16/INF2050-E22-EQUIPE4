69d049997cd36bf7858bc81b1d64a6fc0aed9749
Steny-11 <104903043+Steny-11@users.noreply.github.com>
2022-07-24 19:47:48 -0400

    Junit test pour la classe ValiderValeursLot

--- evaluationfonciere/src/test/java/com/proudmusketeers/ValiderValeursLotTest.java
+++ evaluationfonciere/src/test/java/com/proudmusketeers/ValiderValeursLotTest.java
@@ -2,48 +2,150 @@ package com.proudmusketeers;
 
 import static org.junit.Assert.*;
 import org.junit.*;
-import org.junit.rules.ExpectedException;
 
 import net.sf.json.JSONObject;
 
 public class ValiderValeursLotTest {
 
-    JSONObject testData = new JSONObject(); 
+    JSONObject testData;
+    ValiderValeursLot validateur;
+
+    @Before
+    public void setUp() throws Exception {
+        testData = new JSONObject();
+        validateur = new ValiderValeursLot(testData);
+    }
 
     @Test (expected = FormatInvalide.class)
-    public void testDescriptionPasDansDocument() throws FormatInvalide {
-        ValiderValeursLot validateur = new ValiderValeursLot(testData);
+    public void testDescriptionManquante() throws FormatInvalide {
         validateur.description();
     }
 
     @Test (expected = FormatInvalide.class)
     public void testDescriptionVide() throws FormatInvalide {
         testData.accumulate("description","");
-        ValiderValeursLot validateur = new ValiderValeursLot(testData);
         validateur.description();
     }
 
     @Test
-    public void testDescriptionValide(){
+    public void testDescriptionValide() throws FormatInvalide {
         testData.accumulate("description", "valide");
-        ValiderValeursLot validateur = new ValiderValeursLot(testData);
+        assertEquals("valide", validateur.description());
 
-        try {
-            assertEquals("valide", validateur.description());
-        } catch (FormatInvalide e) {
-            fail("Ne devrait pas avoir de FormatInvalide exception");
-        }
     }
 
     @Test
-    public void testDescriptionAvecWhitespacesDebutFin(){
+    public void testDescriptionAvecWhitespacesDebutFin() throws FormatInvalide {
         testData.accumulate("description", " valide ");
-        ValiderValeursLot validateur = new ValiderValeursLot(testData);
+        assertEquals("valide", validateur.description());
+    }
 
-        try {
-            assertEquals("valide", validateur.description());
-        } catch (FormatInvalide e) {
-            fail("Ne devrait pas avoir de FormatInvalide exception");
-        }
+    @Test (expected = FormatInvalide.class)
+    public void testNbDroitsPassageManquante() throws FormatInvalide {
+        validateur.nbDroitsPassages();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testNbDroitsPassagePlusQueDix() throws FormatInvalide {
+        testData.accumulate("nombre_droits_passage", 11);
+        validateur.nbDroitsPassages();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testNbDroitsPassageNegatif() throws FormatInvalide {
+        testData.accumulate("nombre_droits_passage", -1);
+        validateur.nbDroitsPassages();
+    }
+
+    @Test
+    public void testNbDroitsPassageValide() throws FormatInvalide {
+        testData.accumulate("nombre_droits_passage", 10);
+        assertEquals(10,validateur.nbDroitsPassages());
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testDateMesureManquante() throws FormatInvalide {
+        validateur.dateMesure();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testDateMesureMauvaiseNotationAnneeJourMois() throws FormatInvalide {
+        testData.accumulate("date_mesure", "2001-30-07");
+        validateur.dateMesure();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testDateMesureMauvaiseNotationJourMoisAnnee() throws FormatInvalide {
+        testData.accumulate("date_mesure", "30-07-2001");
+        validateur.dateMesure();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testDateMesureMauvaiseNotationMoisJourAnnee() throws FormatInvalide {
+        testData.accumulate("date_mesure", "07-30-2001");
+        validateur.dateMesure();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testDateMesureMauvaiseNotationJourAnneeMois() throws FormatInvalide {
+        testData.accumulate("date_mesure", "30-2001-07");
+        validateur.dateMesure();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testDateMesureMauvaiseNotationMoisAnneeJour() throws FormatInvalide {
+        testData.accumulate("date_mesure", "07-2001-30");
+        validateur.dateMesure();
+    }
+
+    @Test
+    public void testDateMesureValide() throws FormatInvalide {
+        testData.accumulate("date_mesure", "2001-07-30");
+        validateur.dateMesure();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testNbServicesManquante() throws FormatInvalide {
+        validateur.nbServices();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testNbServicesPlusQueCinq() throws FormatInvalide {
+        testData.accumulate("nb_services", "6");
+        validateur.nbServices();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testNbServicesNegatif() throws FormatInvalide {
+        testData.accumulate("nb_services", "-1");
+        validateur.nbServices();
+    }
+
+    @Test
+    public void testNbServiceValide() throws FormatInvalide {
+        testData.accumulate("nb_services", "5");
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testSuperficieManquante() throws FormatInvalide {
+        validateur.superficie();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testSuperficiePlusQueCinquanteMilles() throws FormatInvalide {
+        testData.accumulate("superficie", 50001);
+        validateur.superficie();
+    }
+
+    @Test (expected = FormatInvalide.class)
+    public void testSuperficieNegative() throws FormatInvalide {
+        testData.accumulate("superficie", -1);
+        validateur.superficie();
+    }
+
+    @Test
+    public void testSuperficieValide() throws FormatInvalide{
+        testData.accumulate("superficie", 50000);
+        assertEquals(50000, validateur.superficie());
     }
 }
