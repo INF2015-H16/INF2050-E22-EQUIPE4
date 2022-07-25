9aa1e97b6b11a7c6f54ba9f1f93731634ba7d75d
Steny-11 <104903043+Steny-11@users.noreply.github.com>
2022-07-24 20:04:04 -0400

    commentaires pour tests Arrondi

--- evaluationfonciere/src/test/java/com/proudmusketeers/TerrainTest.java
+++ evaluationfonciere/src/test/java/com/proudmusketeers/TerrainTest.java
@@ -6,35 +6,40 @@ import org.junit.*;
 
 public class TerrainTest
 {
-
+    //10.00 -> 10.00
     @Test
     public void testArrondiAuZeroCentimesDirect(){
         assertEquals(10.00, Terrain.arrondiAu5sous(10.00), 0);
     }
 
+    //10.05 -> 10.05
     @Test
     public void testArrondiAuCinqCentimesDirect(){
         assertEquals(10.05, Terrain.arrondiAu5sous(10.05), 0);
     }
 
+    // 9.98 -> 10.00
     @Test
     public void testArrondiAuZeroCentimeVersLeHaut()
     {
         assertEquals(10.00, Terrain.arrondiAu5sous(9.98),  0);
     }
 
+    // 10.02 -> 10.00
     @Test
     public void testArrondiAuZeroCentimeVersLeBas()
     {
         assertEquals(10.00, Terrain.arrondiAu5sous(10.02), 0);
     }
 
+    // 10.03 -> 10.05
     @Test
     public void testArrondiAuCinqCentimesVersLeHaut()
     {
         assertEquals(10.05, Terrain.arrondiAu5sous(10.03), 0);
     }
 
+    // 10.07 -> 10.05
     @Test
     public void testArrondiAuCinqCentimesVersLeBas()
     {
