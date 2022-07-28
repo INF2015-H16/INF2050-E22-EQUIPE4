4a4969b03446fb5c51590ec97d9ee6887bd713fa
Steny-11 <104903043+Steny-11@users.noreply.github.com>
2022-07-27 20:14:54 -0400

    Refactor Junit - verif bon message d'erreur

--- evaluationfonciere/src/test/java/com/proudmusketeers/ValiderValeursLotTest.java
+++ evaluationfonciere/src/test/java/com/proudmusketeers/ValiderValeursLotTest.java
@@ -16,22 +16,29 @@ public class ValiderValeursLotTest {
         validateur = new ValiderValeursLot(testData);
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testDescriptionManquante() throws FormatInvalide {
-        validateur.description();
+    @Test
+    public void testDescriptionManquante() {
+        try {
+            validateur.description();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <description> d'un ou plusieurs lots est manquante dans le fichier d'entree.", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testDescriptionVide() throws FormatInvalide {
-        testData.accumulate("description","");
-        validateur.description();
+    @Test
+    public void testDescriptionVide() {
+        try {
+            testData.accumulate("description","");
+            validateur.description();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <description> de chaque lot ne doit pas etre vide.", e.getMessage());
+        }
     }
 
     @Test
     public void testDescriptionValide() throws FormatInvalide {
         testData.accumulate("description", "valide");
         assertEquals("valide", validateur.description());
-
     }
 
     @Test
@@ -40,21 +47,33 @@ public class ValiderValeursLotTest {
         assertEquals("valide", validateur.description());
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testNbDroitsPassageManquante() throws FormatInvalide {
-        validateur.nbDroitsPassages();
+    @Test
+    public void testNbDroitsPassageManquante() {
+        try{
+            validateur.nbDroitsPassages();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <nombre_droits_passage> d'un ou plusieurs lots est manquante dans le fichier d'entree.", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testNbDroitsPassagePlusQueDix() throws FormatInvalide {
-        testData.accumulate("nombre_droits_passage", 11);
-        validateur.nbDroitsPassages();
+    @Test
+    public void testNbDroitsPassagePlusQueDix() {
+        try{
+            testData.accumulate("nombre_droits_passage", 11);
+            validateur.nbDroitsPassages();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <nombre_droits_passage> de chaque lot ne doit pas depasser 10.", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testNbDroitsPassageNegatif() throws FormatInvalide {
-        testData.accumulate("nombre_droits_passage", -1);
-        validateur.nbDroitsPassages();
+    @Test
+    public void testNbDroitsPassageNegatif(){
+        try{
+            testData.accumulate("nombre_droits_passage", -1);
+            validateur.nbDroitsPassages();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <nombre_droits_passage> de chaque lot ne doit pas etre inferieure a 0.", e.getMessage());
+        }
     }
 
     @Test
@@ -63,39 +82,63 @@ public class ValiderValeursLotTest {
         assertEquals(10,validateur.nbDroitsPassages());
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testDateMesureManquante() throws FormatInvalide {
-        validateur.dateMesure();
+    @Test
+    public void testDateMesureManquante() {
+        try {
+            validateur.dateMesure();
+        } catch (FormatInvalide e){
+            assertEquals("La propriete <date_mesure> d'un ou plusieurs lots est manquante dans le fichier d'entree.", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testDateMesureMauvaiseNotationAnneeJourMois() throws FormatInvalide {
-        testData.accumulate("date_mesure", "2001-30-07");
-        validateur.dateMesure();
+    @Test
+    public void testDateMesureMauvaiseNotationAnneeJourMois(){
+        try {
+            testData.accumulate("date_mesure", "2001-30-07");
+            validateur.dateMesure();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <date_mesure> de chaque lot doit etre dans le format ISO8601 (aaaa-mm-jj).", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testDateMesureMauvaiseNotationJourMoisAnnee() throws FormatInvalide {
-        testData.accumulate("date_mesure", "30-07-2001");
-        validateur.dateMesure();
+    @Test
+    public void testDateMesureMauvaiseNotationJourMoisAnnee() {
+        try {
+            testData.accumulate("date_mesure", "30-07-2001");
+            validateur.dateMesure();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <date_mesure> de chaque lot doit etre dans le format ISO8601 (aaaa-mm-jj).", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testDateMesureMauvaiseNotationMoisJourAnnee() throws FormatInvalide {
-        testData.accumulate("date_mesure", "07-30-2001");
-        validateur.dateMesure();
+    @Test
+    public void testDateMesureMauvaiseNotationMoisJourAnnee() {
+        try {
+            testData.accumulate("date_mesure", "07-30-2001");
+            validateur.dateMesure();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <date_mesure> de chaque lot doit etre dans le format ISO8601 (aaaa-mm-jj).", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testDateMesureMauvaiseNotationJourAnneeMois() throws FormatInvalide {
-        testData.accumulate("date_mesure", "30-2001-07");
-        validateur.dateMesure();
+    @Test
+    public void testDateMesureMauvaiseNotationJourAnneeMois() {
+        try { 
+            testData.accumulate("date_mesure", "30-2001-07");
+            validateur.dateMesure();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <date_mesure> de chaque lot doit etre dans le format ISO8601 (aaaa-mm-jj).", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testDateMesureMauvaiseNotationMoisAnneeJour() throws FormatInvalide {
-        testData.accumulate("date_mesure", "07-2001-30");
-        validateur.dateMesure();
+    @Test
+    public void testDateMesureMauvaiseNotationMoisAnneeJour() {
+        try {
+            testData.accumulate("date_mesure", "07-2001-30");
+            validateur.dateMesure();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <date_mesure> de chaque lot doit etre dans le format ISO8601 (aaaa-mm-jj).", e.getMessage());
+        }
     }
 
     @Test
@@ -104,21 +147,33 @@ public class ValiderValeursLotTest {
         validateur.dateMesure();
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testNbServicesManquante() throws FormatInvalide {
-        validateur.nbServices();
+    @Test
+    public void testNbServicesManquante() {
+        try {
+            validateur.nbServices();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <nombre_services> d'un lot est manquante dans le fichier d'entree.", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testNbServicesPlusQueCinq() throws FormatInvalide {
-        testData.accumulate("nb_services", "6");
-        validateur.nbServices();
+    @Test
+    public void testNbServicesPlusQueCinq() {
+        try {
+            testData.accumulate("nombre_services", "6");
+            validateur.nbServices();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <nombre_services> de chaque lot ne peut pas depasser 5.", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
+    @Test
     public void testNbServicesNegatif() throws FormatInvalide {
-        testData.accumulate("nb_services", "-1");
-        validateur.nbServices();
+        try {
+            testData.accumulate("nombre_services", "-1");
+            validateur.nbServices();
+        } catch (FormatInvalide e) {
+            assertEquals("Le propriete <nombre_services> de chaque lot ne peut pas etre inferieure a 0.", e.getMessage());
+        }
     }
 
     @Test
@@ -126,21 +181,33 @@ public class ValiderValeursLotTest {
         testData.accumulate("nb_services", "5");
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testSuperficieManquante() throws FormatInvalide {
-        validateur.superficie();
+    @Test 
+    public void testSuperficieManquante() {
+        try {
+            validateur.superficie();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <superficie> d'un lot est manquante dans le fichier d'entree.", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testSuperficiePlusQueCinquanteMilles() throws FormatInvalide {
-        testData.accumulate("superficie", 50001);
-        validateur.superficie();
+    @Test
+    public void testSuperficiePlusQueCinquanteMilles() {
+        try { 
+            testData.accumulate("superficie", 50001);
+            validateur.superficie();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <superficie> de chaque lot ne doit pas depasser 50000 metres carres.", e.getMessage());
+        }
     }
 
-    @Test (expected = FormatInvalide.class)
-    public void testSuperficieNegative() throws FormatInvalide {
-        testData.accumulate("superficie", -1);
-        validateur.superficie();
+    @Test 
+    public void testSuperficieNegative() {
+        try{
+            testData.accumulate("superficie", -1);
+            validateur.superficie();
+        } catch (FormatInvalide e) {
+            assertEquals("La propriete <superficie> de chaque lot ne doit pas etre negative.", e.getMessage());
+        }
     }
 
     @Test
